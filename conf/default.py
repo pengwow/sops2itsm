ea42e0f4766a84a268b936de98096abb0a13f928
pengwow <pengwow@hotmail.com>
2019-07-21 19:54:12 +0800

    update

--- conf/default.py
+++ conf/default.py
@@ -22,8 +22,8 @@ from django.conf.global_settings import *  # noqa
 # 应用基本信息配置 (请按照说明修改)
 # ==============================================================================
 # 在蓝鲸智云开发者中心 -> 点击应用ID -> 基本信息 中获取 APP_ID 和 APP_TOKEN 的值
-APP_ID = 'liupeng-saas'
-APP_TOKEN = 'dd9ef3a0-cb72-4c85-8283-33ec9e82e246'
+APP_ID = 'sops2itsm'
+APP_TOKEN = '83ed5c1b-8d61-4e5f-9e2b-5cfb7afcb3e6'
 # 蓝鲸智云开发者中心的域名，形如：http://paas.bking.com
 BK_PAAS_HOST = 'https://paas.bjbk.com/'
 
